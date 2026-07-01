4be04f062a3ae7068705fb88c52e287e65f522e7
samiakrim67-ai <samiakrim67@gmail.com>
2026-07-01 23:48:44 +0100

    First upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container");
+const seal = document.getElementById("seal");
+
+seal.addEventListener("click", ()=>{
+
+    container.classList.add("open");
+
+});
